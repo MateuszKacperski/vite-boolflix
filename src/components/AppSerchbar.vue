43f76b22fbe5d1be6061575bf5a6a812abc07862
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-02-07 13:19:58 +0100

    header components

--- src/components/AppSerchbar.vue
+++ src/components/AppSerchbar.vue
@@ -4,6 +4,11 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Film">
+        <button class="btn btn-outline-secondary" type="button">Button</button>
+    </div>
+</template>
 
 <style lang="scss" scoped></style>
